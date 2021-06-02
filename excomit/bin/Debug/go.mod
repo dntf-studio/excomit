15639fc201661c6777d8f5c380d9030ae820b46e
dntf <*****@users.noreply.github.com>
2021-06-02 21:43:50 +0900

    0.7

---
+++ excomit/bin/Debug/go.mod
@@ -0,0 +1,9 @@
+module goex/mods
+
+go 1.16
+
+require (
+	github.com/fatih/color v1.12.0
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	golang.org/x/sys v0.0.0-20210601080250-7ecdf8ef093b // indirect
+)
--- excomit/bin/Debug/reader.go
+++ excomit/bin/Debug/reader.go
@@ -2,42 +2,88 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("a")
+
+reload:
 	var list []string
-	err, list := read(".txt")
+	list, err := read(".txt")
 	if err != nil {
 		fmt.Println("Eorror was detected")
 		os.Exit(1)
 	}
 
+	w1 := color.New(color.FgHiGreen)
+	w2 := color.New(color.FgHiYellow, color.Underline)
 	for i, v := range list {
 		comm := strings.Split(v, ",")
 		if i == 0 {
-			fmt.Println("_________________________________")
-			fmt.Println("| ", i, "  ", comm[0], "  ", comm[1])
+			w1.Println("_________________________________")
+			w1.Println("| ", i, "  ", comm[0], "  ", comm[1])
 		} else if i < 10 {
-			fmt.Println("| ", i, "  ", comm[0], "  ", comm[1])
+			w1.Println("| ", i, "  ", comm[0], "  ", comm[1])
 		} else {
-			fmt.Println("|", i, "  ", comm[0], "  ", comm[1])
+			w1.Println("|", i, "  ", comm[0], "  ", comm[1])
 		}
 	}
-
-	fmt.Print(">>")
+	w2.Println("\n               $$残り人数: ", len(list), "人$$              ")
+	w1.Add(color.Underline)
+	w1.Print("\n>>")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
+	str := scanner.Text()
+	str = strings.TrimSpace(str)
+
+	s, e := judgeman(str)
+	if e != nil {
+		fmt.Println("error: ", e.Error())
+		confilm()
+	} else {
+		if s == "r" {
+			goto reload
+		}
+	}
 }
 
-func read(fn string) (error, []string) {
+func judgeman(s string) (string, error) {
+	if strings.ToLower(s) == "r" {
+		return "r", nil
+	} else {
+		err := errors.New("incorrect command")
+		return "", err
+	}
+}
+
+func confilm() {
+	w3 := color.New(color.FgHiRed)
+	w3.Print("are you sure quit?(Y/N) ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	s := scanner.Text()
+	s = strings.TrimSpace(s)
+
+	if strings.ToUpper(s) == "Y" {
+		os.Exit(0)
+	} else if strings.ToUpper(s) == "N" {
+		main()
+	} else {
+		w3.Println("plz put in with 'Y' or 'N'")
+		confilm()
+	}
+}
+
+func read(fn string) ([]string, error) {
 	var list []string
 	file, err := os.Open(fn)
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 	defer file.Close()
 
@@ -48,8 +94,8 @@ func read(fn string) (error, []string) {
 	}
 
 	if err := reader.Err(); err != nil {
-		return err, nil
+		return nil, err
 	}
 
-	return nil, list
+	return list, nil
 }
